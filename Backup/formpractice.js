ff7c86cd0baab9cc7016736deb4e6c3d7f198c73
JOSH-ITHA <upatijoshitha@gmail.com>
2026-07-03 12:07:26 +0530

    handle form request

--- Backup/formpractice.js
+++ Backup/formpractice.js
@@ -1,5 +1,7 @@
 const http=require('http');
 const fs=require('fs');
+const querystring=require('querystring');
+
 http.createServer((req,res)=>{
     fs.readFile('from.html','utf8',(err,data)=>{
         if(err){
@@ -9,16 +11,30 @@ http.createServer((req,res)=>{
         }
         else if(req.url=='/'){
         res.writeHead(200,{'Content-Type':'text/html'});
+        }
+        if(req.url=='/'){
         res.write(data);
         }else if(req.url=='/submit'){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write("<h1>Form Submitted Succesfully</h1>");
+            let userData=[];
+            req.on('data',(chunk)=>{
+                userData.push(chunk);
+            });
+            req.on("end",()=>{
+                let parseData=Buffer.concat(userData).toString();
+                let readableData=querystring.parse(parseData);
+                //console.log(readableData);
+                let dataFinal=`Name: ${readableData.username},  Email: ${readableData.email} `;
+                //console.log(dataFinal);
+                fs.writeFileSync('tata.txt',dataFinal);
+            })
+            console.log(userData);
+            res.write("<h1>Form Submitted Successfully</h1>");
         }else{
-            res.writeHead(404,{'content-type':'text/plain'});
-            res.end("404 Not found");
+            res.write("<h1>404 Not Found</h1>");
+           
         }
         res.end();
         
-    });
+});
     
 }).listen(3000);
