0d86b6c69b71a45b7ec7f24f69bcb5cb882b2725
JOSH-ITHA <upatijoshitha@gmail.com>
2026-07-06 13:05:35 +0530

    private key

---
+++ Backup/https-module.js
@@ -0,0 +1,21 @@
+// const http=require('http');
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end('Hello world!\n');
+
+// }).listen(3000,()=>{
+//     console.log('Server running at http://localhost:3000/');
+// });
+const https=require('https');
+const fs=require('fs');
+const options={
+    key:fs.readFileSync('key.pem'),
+    cert:fs.readFileSync('cert.pem'),
+};
+const server=https.createServer(options,(req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end("<h1>Hello Secure World!</h1>\n");
+});
+server.listen(3000,()=>{
+    console.log("HTTPS Server running at https://localhost:3000/");
+});                         
